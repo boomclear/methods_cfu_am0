7451cf19a30e38ab8b01833141aac54036729e8f
Austin Woods <austinwoods99@gmail.com>
2023-02-23 16:06:38 -0500

    Complete built_in_nethods.rb

--- built_in_methods.rb
+++ built_in_methods.rb
@@ -10,19 +10,24 @@
 
 
 "Hello World".include?("Hello")
-
+# This method returns a boolean whether the string contains the arguement or not
+# The return value is true
 
 "Hello World".end_with?("Hello")
-
+# This method returns a boolean whether the string ends with the arguement or not
+# the return value is false
 
 "Hello World".end_with?("rld")
-
+# This method returns a boolean whether the string ends with the arguement or not
+# the return value is true
 
 12.even?
-
+# This method calls on an integer to see if it is even or not.
+# it returns a boolean
 
 18.next
-
+# this method returns an Integer + 1
+# ex: 1.next is == 2
 
 
 # SECTION 2: Calling methods on variables assigned to strings.
@@ -39,18 +44,42 @@
 first_name = "Jeff"
 puts first_name.start_with?("J")
 
-
+dog = "Mouse"
+puts dog.swapcase!
+#This method swaps all the case for each letter
+# Returns in this case: "mOUSE"
+name = "Austin"
+puts name.reverse!
+# this method reverse the letters in a string
+# in this case: "nitsuA"
 
 # SECTION 3: Calling methods on variables assigned to integers.
 # Declare 2 variables assigned to integer objects.
 # Call a different built-in Ruby method on each of your variables. 
 # https://ruby-doc.org/core-3.1.0/Integer.html
 # Include comments above each method call explaining the impact and return value of that method.
+ age = 23
+ age.size
+ # Returns the number of bytes in the machine representation of the Integer
+ # in this case: 8
 
-
+ weight = 205
+weight.to_s
+#This method converts an Integer into a String
+# 205 becomes "205"
 
 # SECTION 4: Calling methods on variables assigned to arrays.
 # Declare 2 variables assigned to arrays.
 # Call a different built-in Ruby method on each of your variables. 
 # https://ruby-doc.org/core-3.1.0/Array.html
 # Include comments above each method call explaining the impact and return value of that method.
+
+
+numbers = [1, 2, 3]
+numbers.reverse
+#this method reverses the order of the elements in the array
+# in this case: [3, 2, 1]
+grass = ["short", "long"]
+grass.count
+#this method counts the elements in an array
+# in this case 2
